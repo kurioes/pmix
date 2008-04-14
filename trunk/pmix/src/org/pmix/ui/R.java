5cb11c0e1e95d023fda8bb43f76de887766a0bf2
rflament@laposte.net <rflament@laposte.net@2e0d6e4e-4b40-0410-bb0c-158b58bef44a>
2008-04-14 15:27:30 +0000

    amazon covers support

--- trunk/pmix/src/org/pmix/ui/R.java
+++ trunk/pmix/src/org/pmix/ui/R.java
@@ -11,12 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int even_stripe=0x7f020001;
-        public static final int icon=0x7f020000;
-        public static final int odd_stripe=0x7f020002;
-        public static final int solid_blue=0x7f020004;
-        public static final int solid_green=0x7f020005;
-        public static final int solid_red=0x7f020003;
+        public static final int even_stripe=0x7f020002;
+        public static final int gmpcnocover=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int odd_stripe=0x7f020003;
+        public static final int solid_blue=0x7f020005;
+        public static final int solid_green=0x7f020006;
+        public static final int solid_red=0x7f020004;
     }
     public static final class id {
         public static final int albumCover=0x7f050004;
