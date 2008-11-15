a8e96b9967c9f14fdd84f088fbc553a8333f12f9
stefan.agner <stefan.agner@2e0d6e4e-4b40-0410-bb0c-158b58bef44a>
2008-11-15 23:42:16 +0000

    - Fixed bug when deleting second last and last Playlist entry - Updated String resources, general Code cleanup

--- trunk/pmix/src/org/pmix/ui/SettingsActivity.java
+++ trunk/pmix/src/org/pmix/ui/SettingsActivity.java
@@ -1,12 +1,7 @@
 package org.pmix.ui;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class SettingsActivity extends PreferenceActivity {
 	
@@ -14,29 +9,7 @@ public class SettingsActivity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.settings);
-		
-		//SharedPreferences pref = getSharedPreferences("org.pmix", MODE_PRIVATE);
-		//pref.
-		//EditTextPreference host = (EditTextPreference)findPreference("hostname");
 		setPersistent(true);
-		//getSharedPreferences("org.pmix", MODE_PRIVATE);
-		
-		//host.setText(Settings.getInstance().getServerAddress());
-		/*
-		setContentView(R.layout.settings);
-		final EditText editText = (EditText) findViewById(R.id.serverAddress);
-		
-		editText.setText();
-		
-		Button button = (Button) findViewById(R.id.ok);
-		button.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				Settings.getInstance().setServerAddress(editText.getText().toString());
-				finish();
-				
-			}
-		});
-		*/
 	}
 	
 }
